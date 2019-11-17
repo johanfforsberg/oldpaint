ac14152b1de74b5f2bb108159ca3de92b11e7148
Johan Forsberg <johan@slentrian.org>
2019-11-17 09:33:20 +0100

    Eventloop customization.

--- oldpaint/__main__.py
+++ oldpaint/__main__.py
@@ -6,9 +6,20 @@ pyximport.install()  # Setup cython to autocompile pyx modules
 from .window import OldpaintWindow
 
 
-config = pyglet.gl.Config(major_version=4, minor_version=5,
+config = pyglet.gl.Config(major_version=4, minor_version=5,  # Minimum OpenGL requirement
                           double_buffer=False)  # Double buffering gives noticable cursor lag
 
 window = OldpaintWindow(width=800, height=600, config=config)
 
-pyglet.app.run()
+
+class OldpaintEventLoop(pyglet.app.EventLoop):
+
+    "A tweaked event loop that lowers the idle refresh rate for less CPU heating."
+
+    def idle(self):
+        super().idle()
+        return 0.1
+
+
+pyglet.app.event_loop = OldpaintEventLoop()
+pyglet.app.run(0.01)  # TODO why is this argument needed?
