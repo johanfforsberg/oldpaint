ceb6af2488610b566d117168b5886ffdc1dfc0d2
Johan Forsberg <johan@slentrian.org>
2022-01-23 19:01:54 +0100

    Set window size in plugin

--- plugins/power_palette.py
+++ plugins/power_palette.py
@@ -3,6 +3,8 @@ from oldpaint.ui.colors import PaletteColors
 
 class Plugin:
 
+    window_size = (420, 550)
+
     def __init__(self):
         self.selection = None
         self.palette_colors = PaletteColors()
