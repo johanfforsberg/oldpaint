7245af3c8b5e54e93b869ced9cce0ed7376b5d2b
Johan Forsberg <johan@slentrian.org>
2022-03-22 22:33:30 +0100

    Tests

--- tests/test_layer.py
+++ tests/test_layer.py
@@ -1,9 +1,13 @@
 import numpy as np
 
 from oldpaint import layer, brush
+from oldpaint.palette import Palette
 
 
 def test_draw_line():
-    l = layer.Layer(size=(100, 100))
-    b = brush.EllipseBrush()
-    l.draw_line((20, 30), (70, 80), b.data, (0, 0))
+    l = layer.BackupLayer(size=(100, 100))
+    palette = Palette([(0, 0, 0, 0), (255, 255, 255, 255)])
+    b = brush.SquareBrush()
+    print(b.data)
+    l.draw_line((20, 30), (70, 80), b.data, offset=(0, 0))
+    l.save_png("/tmp/layer.png", palette, 0)
